5a71cfaa172403e0fdf256797dc667a55374f557
Leonardo Borges <leonardoborges.rj@gmail.com>
2012-09-13 17:51:53 +1000

    Updated the project file so it works with leiningen 2

--- project.clj
+++ project.clj
@@ -1,19 +1,17 @@
-(defproject storm-starter "0.0.1-SNAPSHOT"
-  :source-path "src/clj"
-  :java-source-path "src/jvm"
-  :javac-options {:debug "true" :fork "true"}
-  :resources-path "multilang"
-  :aot :all
-  :repositories {
-;;                 "twitter4j" "http://twitter4j.org/maven2"
-                 }
-
+(defproject storm-starter/storm-starter "0.0.1-SNAPSHOT"
   :dependencies [
-;;                 [org.twitter4j/twitter4j-core "2.2.6-SNAPSHOT"]
-;;                 [org.twitter4j/twitter4j-stream "2.2.6-SNAPSHOT"]
-                 ]
-
-  :dev-dependencies [[storm "0.8.1"]
-                     [org.clojure/clojure "1.4.0"]
-                     ])
-
+;;    [org.twitter4j/twitter4j-core "2.2.6-SNAPSHOT"]
+;;    [org.twitter4j/twitter4j-stream "2.2.6-SNAPSHOT"]
+  ]
+  :source-paths ["src/clj"]
+  :profiles {:dev
+             {:dependencies
+              [[storm "0.8.1"] [org.clojure/clojure "1.4.0"]]}}
+  :repositories {
+;;    "twitter4j" "http://twitter4j.org/maven2"
+  }
+  :resource-paths ["multilang"]
+  :aot :all
+  :java-source-paths ["src/jvm"]
+  :min-lein-version "2.0.0"
+  :javac-options {:debug "true"})
