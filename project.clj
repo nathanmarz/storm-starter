338769b4e7ce1b1ac3aaa4fcd3e3c1d5c60554a3
zsxwing <zsxwing@gmail.com>
2013-07-09 14:59:26 +0800

    Add scala library to the jar so that it can be run without scala environment.

--- project.clj
+++ project.clj
@@ -12,12 +12,12 @@
 ;;                 [org.twitter4j/twitter4j-core "2.2.6-SNAPSHOT"]
 ;;                 [org.twitter4j/twitter4j-stream "2.2.6-SNAPSHOT"]
                    [commons-collections/commons-collections "3.2.1"]
+                   [org.scala-lang/scala-library "2.10.1"]
                  ]
 
   :profiles {:dev
               {:dependencies [[storm "0.8.2"]
-                              [org.clojure/clojure "1.4.0"]
-                              [org.scala-lang/scala-library "2.10.1"]]}}
+                              [org.clojure/clojure "1.4.0"]]}}
   :min-lein-version "2.0.0"
   :plugins [[lein-scalac "0.1.0"]]
   :prep-tasks ["scalac"]
