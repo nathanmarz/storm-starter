ccb810315ab577ab416813ce7cc2773d8e201f4b
Prabeesh K <prabsmails@gmail.com>
2013-10-21 12:38:05 +0530

    Update twitter auth for twiter4j API v2.0.

--- src/jvm/storm/starter/spout/TwitterSampleSpout.java
+++ src/jvm/storm/starter/spout/TwitterSampleSpout.java
@@ -1,11 +1,6 @@
-/*
-
 package storm.starter.spout;
 
 import backtype.storm.Config;
-import twitter4j.conf.ConfigurationBuilder;
-import twitter4j.TwitterStream;
-import twitter4j.TwitterStreamFactory;
 import backtype.storm.spout.SpoutOutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
@@ -15,91 +10,135 @@ import backtype.storm.tuple.Values;
 import backtype.storm.utils.Utils;
 import java.util.Map;
 import java.util.concurrent.LinkedBlockingQueue;
+import twitter4j.FilterQuery;
+import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.conf.ConfigurationBuilder;
 
+@SuppressWarnings("serial")
 public class TwitterSampleSpout extends BaseRichSpout {
-    SpoutOutputCollector _collector;
-    LinkedBlockingQueue<Status> queue = null;
-    TwitterStream _twitterStream;
-    String _username;
-    String _pwd;
-    
-    
-    public TwitterSampleSpout(String username, String pwd) {
-        _username = username;
-        _pwd = pwd;
-    }
-    
-    @Override
-    public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
-        queue = new LinkedBlockingQueue<Status>(1000);
-        _collector = collector;
-        StatusListener listener = new StatusListener() {
 
-            @Override
-            public void onStatus(Status status) {
-                queue.offer(status);
-            }
+	SpoutOutputCollector _collector;
+	LinkedBlockingQueue<Status> queue = null;
+	TwitterStream _twitterStream;
+	String consumerKey;
+	String consumerSecret;
+	String accessToken;
+	String accessTokenSecret;
+	String[] keyWords;
 
-            @Override
-            public void onDeletionNotice(StatusDeletionNotice sdn) {
-            }
+	public TwitterSampleSpout(String ConsumerKey, String ConsumerSecret,
+			String AccessToken, String AccessTokenSecret, String[] KeyWords) {
+		this.consumerKey = ConsumerKey;
+		this.consumerSecret = ConsumerSecret;
+		this.accessToken = AccessToken;
+		this.accessTokenSecret = AccessTokenSecret;
+		this.keyWords = KeyWords;
+	}
 
-            @Override
-            public void onTrackLimitationNotice(int i) {
-            }
+	public TwitterSampleSpout() {
+		// TODO Auto-generated constructor stub
+	}
 
-            @Override
-            public void onScrubGeo(long l, long l1) {
-            }
+	@Override
+	public void open(Map conf, TopologyContext context,
+			SpoutOutputCollector collector) {
+		queue = new LinkedBlockingQueue<Status>(1000);
+		_collector = collector;
 
-            @Override
-            public void onException(Exception e) {
-            }
-            
-        };
-        TwitterStreamFactory fact = new TwitterStreamFactory(new ConfigurationBuilder().setUser(_username).setPassword(_pwd).build());
-        _twitterStream = fact.getInstance();
-        _twitterStream.addListener(listener);
-        _twitterStream.sample();
-    }
+		StatusListener listener = new StatusListener() {
 
-    @Override
-    public void nextTuple() {
-        Status ret = queue.poll();
-        if(ret==null) {
-            Utils.sleep(50);
-        } else {
-            _collector.emit(new Values(ret));
-        }
-    }
+			@Override
+			public void onStatus(Status status) {
+				// status.getUser().getName();
+				queue.offer(status);
+			}
 
-    @Override
-    public void close() {
-        _twitterStream.shutdown();
-    }
+			@Override
+			public void onDeletionNotice(StatusDeletionNotice sdn) {
+			}
 
-    @Override
-    public Map<String, Object> getComponentConfiguration() {
-        Config ret = new Config();
-        ret.setMaxTaskParallelism(1);
-        return ret;
-    }    
+			@Override
+			public void onTrackLimitationNotice(int i) {
+			}
 
-    @Override
-    public void ack(Object id) {
-    }
+			@Override
+			public void onScrubGeo(long l, long l1) {
+			}
 
-    @Override
-    public void fail(Object id) {
-    }
+			@Override
+			public void onException(Exception ex) {
+			}
 
-    @Override
-    public void declareOutputFields(OutputFieldsDeclarer declarer) {
-        declarer.declare(new Fields("tweet"));
-    }
-    
+			@Override
+			public void onStallWarning(StallWarning arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+		};
+
+		TwitterStream twitterStream = new TwitterStreamFactory(
+				new ConfigurationBuilder().setJSONStoreEnabled(true).build())
+				.getInstance();
+
+		twitterStream.addListener(listener);
+		twitterStream.setOAuthConsumer(consumerKey, consumerSecret);
+		AccessToken token = new AccessToken(accessToken, accessTokenSecret);
+		twitterStream.setOAuthAccessToken(token);
+		
+		if (keyWords.length == 0) {
+
+			twitterStream.sample();
+		}
+
+		else {
+
+			FilterQuery query = new FilterQuery().track(keyWords);
+			twitterStream.filter(query);
+		}
+
+	}
+
+	@Override
+	public void nextTuple() {
+		Status ret = queue.poll();
+		if (ret == null) {
+			Utils.sleep(50);
+		} else {
+			_collector.emit(new Values(ret));
+
+		}
+	}
+
+	@Override
+	public void close() {
+		_twitterStream.shutdown();
+	}
+
+	@Override
+	public Map<String, Object> getComponentConfiguration() {
+		Config ret = new Config();
+		ret.setMaxTaskParallelism(1);
+		return ret;
+	}
+
+	@Override
+	public void ack(Object id) {
+	}
+
+	@Override
+	public void fail(Object id) {
+	}
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declare(new Fields("tweet"));
+	}
+
 }
-*/
